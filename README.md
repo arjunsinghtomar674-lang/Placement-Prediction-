366f43a50e37b83fdb19efd224c45e344456a828
Arjun Singh Tomar <arjunsinghtomar674@gmail.com>
2026-08-18 23:34:28 +0530

    Organize placement prediction project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Placement Prediction
+
+## Project Overview
+
+This project uses Machine Learning to predict student placement outcomes based on different academic and personal features.
+
+## Project Structure
+
+```text
+Placement-Prediction/
+│
+├── data/
+├── notebook/
+├── model/
+├── app.py
+├── README.md
+├── requirements.txt
+└── .gitignore
---
+++ app.py
@@ -0,0 +1,215 @@
+#-------------------------------
+## STEP.1 -> Import Libraries
+#-------------------------------
+
+import pandas as pd 
+import streamlit as st
+import joblib
+
+
+#------------------------------
+## STEP.2 -> Load the model
+#------------------------------
+
+model = joblib.load("placement_model.joblib")
+scaler = joblib.load("scaler.joblib")
+
+
+#-----------------------------------
+## STEP.3 -> Title and Subheader 
+#-----------------------------------
+
+# Title of the APP
+st.title("PLACEMENT PREDICTOR")
+
+# Purpose 
+st.subheader("Predict the Student is placed or not")
+
+# Process 
+st.write("Enter the details of the student given below and predict the output")
+
+# Image
+st.image("pexels-tima-miroshnichenko-5439368.jpg",width = 400)
+
+
+
+#-------------------------
+## STEP.4 -> USER INPUTS
+#-------------------------
+
+
+# student name
+name = st.text_input("Enter your name")
+st.write(f"The name is {name}")
+
+# Use feature of actual datasets for inputs
+
+# 1. CGPA 
+cgpa = st.number_input("The CGPA is",
+                       min_value = 2.00,
+                       max_value = 10.00,
+                       value = 5.00,
+                       step = 0.01)
+
+st.write(f"The CGPA is {cgpa}")
+
+
+# 2. Internship 
+intern = st.slider(" Internship ",0,2,5,1)
+st.write(f"Internship : {intern}")
+
+
+# 3. Projects
+project  = st.slider("project",0,2,5,1)
+st.write(f"Project : {project}")
+
+
+# 4. workshop certification 
+workshop = st.slider("Workshop certification ",0,2,5,1)
+st.write(f"Workshop Certifoication : {workshop}")
+
+
+# 5. Aptitude Score 
+aptitude = st.number_input("Aptitude Score ",
+                           min_value = 10,
+                           max_value = 100,
+                           value = 30,
+                           step = 1)
+
+st.write(f"The Aptitude Score is {aptitude}")
+
+
+# 6. Soft Skill Rating 
+softskill = st.number_input("SoftSkillRating",
+                            min_value = 1.00,
+                            max_value = 5.00,
+                            value = 2.00,
+                            step = 0.01)
+st.write(f"The SoftSkillRating is {softskill}")
+
+
+# 7. ExtraCricularActivities
+eca = st.selectbox("ExtraCuricularActivities",["Yes","No"])
+
+
+# 8. Placement Training 
+placement = st.selectbox("Placement Training ",["Yes","No"])
+
+
+# 9. SSC marks
+ssc = st.number_input("SSC marks ",
+                      min_value = 33,
+                      max_value = 100,
+                      value = 40,
+                      step = 1)
+
+st.write(f"SSC marks is {ssc}")
+
+
+
+# 10. HSC marks
+hsc = st.number_input("HSC marks",
+                      min_value = 33,
+                      max_value = 100,
+                      value = 55,
+                      step = 1)
+
+st.write(f"HSC marks {hsc}")
+
+
+
+
+#---------------------------------------------------------
+## STEP.5 -> Convert the user input into the dataframes
+#---------------------------------------------------------
+
+input_data = pd.DataFrame({
+    "CGPA":[cgpa],
+    "Internships":[intern],
+    "Projects":[project],
+    "Workshops/Certifications":[workshop],
+    "AptitudeTestScore":[aptitude],
+    "SoftSkillsRating":[softskill],
+    "ExtracurricularActivities":[eca],
+    "PlacementTraining":[placement],
+    "SSC_Marks":[ssc],
+    "HSC_Marks":[hsc],
+})
+
+input_data = pd.get_dummies(
+    input_data,
+    columns=["ExtracurricularActivities", "PlacementTraining"],
+    dtype=int
+)
+
+for col in [
+    "ExtracurricularActivities_No",
+    "ExtracurricularActivities_Yes",
+    "PlacementTraining_No",
+    "PlacementTraining_Yes"
+]:
+    if col not in input_data:
+        input_data[col] = 0
+
+
+input_data["StudentID"] = 0
+
+input_data = input_data[
+    [
+        "StudentID",
+        "CGPA",
+        "Internships",
+        "Projects",
+        "Workshops/Certifications",
+        "AptitudeTestScore",
+        "SoftSkillsRating",
+        "SSC_Marks",
+        "HSC_Marks",
+        "ExtracurricularActivities_No",
+        "ExtracurricularActivities_Yes",
+        "PlacementTraining_No",
+        "PlacementTraining_Yes"
+    ]
+]
+
+num_cols = [
+    "CGPA",
+    "Internships",
+    "Projects",
+    "Workshops/Certifications",
+    "AptitudeTestScore",
+    "SoftSkillsRating",
+    "SSC_Marks",
+    "HSC_Marks"
+]
+
+input_data[num_cols] = scaler.transform(input_data[num_cols])
+
+
+
+
+#-----------------------
+## STEP.6 -> Prediction
+#-----------------------
+
+if st.button("🚀 Predict Score "):
+
+    prediction = model.predict(input_data)
+
+    probability = model.predict_proba(input_data)[0][1]
+
+    st.write(f"Name : {name}")
+
+    if prediction[0] == 1:
+
+        st.success("You are Placed")
+        st.image("pexels-ersin-1398102958-31689192.jpg",width = 100)
+
+    else:
+        st.error("You are not Placed")
+        st.image("pexels-mikhail-nilov-7534380.jpg")
+
+    st.metric(
+        "Placement Probability ",
+        f"{probability * 100:.2f}%"
+    )
